d0225717d5af7153ba4d9c47c9b5226c1ab991ba
Christopher Parsons <cparsons@habaneroconsulting.com>
2017-12-16 14:55:44 -0500

    Update 2017-12-16T19:55:44.248Z

---
+++ scripts/scripts.js
@@ -0,0 +1,10 @@
+/**
+ * cwparsons.ca
+ */
+(function () {
+	'use strict';
+
+	// Remove no-js classname
+	var html = document.getElementsByTagName('html')[0];
+	html.className = html.className.replace('no-js', '');
+})();
